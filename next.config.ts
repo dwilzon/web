271ce534ca1a57b91693eafefbd1796a4124ebd4
David Wilson <dwilzon@gmail.com>
2026-03-01 12:56:45 -0600

    Fix formatting and remove unnecessary comment

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  FOR THE LOVE OF GOD WORK /* config options here */
-  reactCompiler: true,
+  /* config options here */
 };
 
 export default nextConfig;
